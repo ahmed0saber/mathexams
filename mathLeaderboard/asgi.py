828ea00ab0d4392b3e0af09f8c5369db008b01fe
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-06-17 18:43:28 +0200

    second commit

---
+++ mathLeaderboard/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mathLeaderboard project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mathLeaderboard.settings')
+
+application = get_asgi_application()
