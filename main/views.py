78ea89993a0aaab4751316836eb720ce34fff968
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-06-17 20:39:03 +0200

    operator /

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from .models import hero
 # Create your views here.
 def index(request):
     if request.method == 'GET':
-        heros = hero.objects.all()
+        heros = hero.objects.all().order_by('-score')
         return render(request, 'main/index.html', {'heros':heros})
     else:
         name = request.POST.get('name')
